21f057d32b4fe7f25c819bd502425da754aa8cee
unknown <isaacgamaliel567@gmail.com>
2022-10-03 19:52:12 -0500

    first commit

---
+++ ProyectoAndroid/ProyectoAndroid/Services/ApiRest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace ProyectoAndroid.Services
+{
+    class ApiRest
+    {
+        //Url donde se consume la api creada
+        private string URL = "utl";
+    }
+}
