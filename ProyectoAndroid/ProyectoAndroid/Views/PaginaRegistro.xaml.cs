7120f5e62f2822a6b64fa81e40e4ccac1e7adda8
unknown <rojashernandezmiguel237@gmail.com>
2022-10-21 17:18:21 -0500

    Validaciones en registro

--- ProyectoAndroid/ProyectoAndroid/Views/PaginaRegistro.xaml.cs
+++ ProyectoAndroid/ProyectoAndroid/Views/PaginaRegistro.xaml.cs
@@ -19,6 +19,57 @@ namespace ProyectoAndroid.Views
             pkGenero.Items.Add("Femenino");
         }
 
-       
+        private async void RegistroButton_Clicked (object sender, EventArgs e)
+        {
+            //Validación Nombre
+            if (string.IsNullOrWhiteSpace(Nombre.Text))
+            {
+                await DisplayAlert("Advertencia", "El campo del correo es obligatorio", "Ok");
+                return;
+            }
+            //Validación Apellido
+            if (string.IsNullOrWhiteSpace(Apellido.Text))
+            {
+                await DisplayAlert("Advertencia", "El campo del contraseña es obligatorio", "Ok");
+                return;
+            }
+            //Validación Apodo
+            if (string.IsNullOrWhiteSpace(NickName.Text))
+            {
+                await DisplayAlert("Advertencia", "El campo del contraseña es obligatorio", "Ok");
+                return;
+            }
+            //Validación Correo
+            if (string.IsNullOrWhiteSpace(correo.Text))
+            {
+                await DisplayAlert("Advertencia", "El campo del contraseña es obligatorio", "Ok");
+                return;
+            }
+            //Validación contraseña
+            if (string.IsNullOrWhiteSpace(contraseña.Text))
+            {
+                await DisplayAlert("Advertencia", "El campo del contraseña es obligatorio", "Ok");
+                return;
+            }
+
+            //Validación Confirmación de contraseña
+            if (string.IsNullOrWhiteSpace(ConfirmarContra.Text))
+            {
+                await DisplayAlert("Advertencia", "El campo del contraseña es obligatorio", "Ok");
+                return;
+            }
+            //Validación de genero
+            if (pkGenero.SelectedIndex == -1)
+            {
+                await DisplayAlert("Error", "Seleccine su género", "OK");
+                return;
+            }
+
+            
+
+           
+
+
+        }
     }
 }
